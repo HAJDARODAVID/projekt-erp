c55cc69bdb0bdb774d322a05b883262e34539a0a
David Hajdarovic <david.hajdarovic@boxmark.com>
2023-12-21 10:12:40 +0100

    test remote

--- resources/views/hidro-projekt/hr.blade.php
+++ resources/views/hidro-projekt/hr.blade.php
@@ -1,3 +1,3 @@
 VIEW test!!
 
-<a href="javascript: w=window.open('{{ route('payrollLabels') }}');">​​​​​​​​​​​​​​​​​print pdf</a>
+<a href="javascript: w=window.open('{{ route('payrollLabels') }}');">PRINTAJ TAJ PDF!!!</a>
