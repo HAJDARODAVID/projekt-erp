9cf224f10b25f65255eb681d2dbcca17ee938fc5
David Hajdarovic <david.hajdarovic@boxmark.com>
2024-01-09 15:48:31 +0100

    fix03

--- resources/views/components/print-label-checkbox.blade.php
+++ resources/views/components/print-label-checkbox.blade.php
@@ -1,3 +1,3 @@
 <div>
-    {{-- <livewire:hidroprojekt.print-label-checkbox wire:key="{{ $row->id }}" row="{{ $row->id }}" value="{{ $row->print_label }}"> --}}
+    <livewire:hidroprojekt.print-label-checkbox wire:key="{{ $row->id }}" row="{{ $row->id }}" value="{{ $row->print_label }}">
 </div>
