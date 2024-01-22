41c64b226fb861e424060c5d7210dcf5da943cb0
David Hajdarovic <david.hajdarovic@boxmark.com>
2024-01-22 11:17:27 +0100

    testing my fix

--- resources/views/components/bde/working-day-entry-card.blade.php
+++ resources/views/components/bde/working-day-entry-card.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="d-flex justify-content-center mb-2" id="test2" onclick="location.href='{{ route('hp_workingDayEntry', $entry->id) }}';" style="cursor: pointer;">   
-        <div clas="panel-body" style="height: 65px; width: 230px;border-radius: 5px; ">@isset($isEntryComplete['cardStyle']) {{$isEntryComplete['cardStyle']}} @endisset
+        <div clas="panel-body" style="height: 65px; width: 230px;border-radius: 5px; @isset($isEntryComplete['cardStyle']) {{$isEntryComplete['cardStyle']}} @endisset">
             <div class="p-2">
                 <b class="d-inline-block text-truncate" style="max-width: 215px;">{{$entry->getConstructionSite->name}}</b><br>
                 <div class="row">
