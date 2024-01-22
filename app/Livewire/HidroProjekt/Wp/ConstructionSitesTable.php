aef3c45dd2daf9d646e1433683e024d6702d7e89
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-01-22 14:23:44 +0100

    formating fix

--- app/Livewire/HidroProjekt/Wp/ConstructionSitesTable.php
+++ app/Livewire/HidroProjekt/Wp/ConstructionSitesTable.php
@@ -37,7 +37,6 @@ class ConstructionSitesTable extends DataTableComponent
                 ->sortable(),
             Column::make("Status", "status")
                 ->sortable(),
-
         ];
     }
 }
