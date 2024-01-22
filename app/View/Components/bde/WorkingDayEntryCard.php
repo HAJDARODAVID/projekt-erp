9ea9b45212b1c83718bc183ea6a90d01c82d409c
David Hajdarovic <david.hajdarovic@boxmark.com>
2024-01-22 10:47:00 +0100

    trying to fix

--- app/View/Components/bde/WorkingDayEntryCard.php
+++ app/View/Components/bde/WorkingDayEntryCard.php
@@ -12,7 +12,7 @@ use App\Services\HidroProjekt\BDE\WorkingDayRecordService;
 class WorkingDayEntryCard extends Component
 {
     public $entry;
-    public $workerCount;
+    public $workerCount = NULL;
     public $isAttendanceComplete;
     public $isEntryComplete;
     /**
@@ -21,8 +21,6 @@ class WorkingDayEntryCard extends Component
     public function __construct($entry)
     {
         $this->entry = $entry;
-
-        dd( $this->entry);
         $this->workerCount = WorkerAttendanceService::getWorkerCount($this->entry->id);
         $this->isAttendanceComplete = WorkerAttendanceService::isAttendanceComplete($this->entry->id);
         $this->isEntryComplete = WorkingDayRecordService::isEntryComplete([$this->isAttendanceComplete]);
