a7f3d8b076d045086874cb035edca6b850373b7d
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-02-01 13:21:39 +0100

    added tab to blade file

--- resources/views/hidro-projekt/HR/allWorkers.blade.php
+++ resources/views/hidro-projekt/HR/allWorkers.blade.php
@@ -1,23 +1,37 @@
 @extends('layouts.mainAdminLayout')
 
 @section('content')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h3">Popis radnika:</h1>
-  </div>
-  
-  <div class="container">
-    @if ($message = Session::get('success'))
+{{-- <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h3">Kadar:</h1>
+  </div> --}}
+
+  <div class="mx-3 mt-3">
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link active"><b>Pregled zaposlenih</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"><b>Popis radnika</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link bi bi-printer" href="javascript: w=window.open('{{ route('hp_payrollLabels') }}');"></a>
+      </li>
+    </ul>
+
+    <div class="mx-5">
+      @if ($message = Session::get('success'))
         <div class="alert alert-success">
             <p>{{ $message }}</p>
         </div>
-    @endif
-    <br>
-    <a class="btn btn-success btn-sm" href="{{ route('hp_newWorkerForm') }}">NOVI RADNIK</a>
-    <a class="btn btn-dark btn-sm" style = "width: 50px" href="javascript: w=window.open('{{ route('hp_payrollLabels') }}');">
-      <i class="bi bi-printer"></i>
-    </a>
-    <hr>
-    <livewire:hidroProjekt.hr.workers-table theme="bootstrap-5" />
+      @endif
+      <br>
 
+      <div class="">
+        <a class="btn btn-success btn-sm" href="{{ route('hp_newWorkerForm') }}">NOVI RADNIK</a>
+        <hr>
+        <livewire:hidroProjekt.hr.workers-table theme="bootstrap-5" />
+      </div>
+      
+    </div>
   </div>
 @endsection
