240f4bfd56727f34d65ea09089c52c6521279bff
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-02-21 14:21:40 +0100

    namespacefix

---
+++ app/View/Components/Hrcopy/WorkerAttendanceAbsenceReason.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\View\Components\hr;
+
+use App\Models\AttendanceModel;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class WorkerAttendanceAbsenceReason extends Component
+{
+    public $absenceShtTxt;
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        $this->absenceShtTxt = AttendanceModel::ABSENCE_REASON;
+        dd($this->absenceShtTxt);
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.hr.worker-attendance-absence-reason');
+    }
+}
