6aa33b09ceea1afd8ef2beb2f046ca2b500ef08a
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-02-23 08:28:32 +0100

    happy b-day card home page

--- resources/views/hidro-projekt/admin.blade.php
+++ resources/views/hidro-projekt/admin.blade.php
@@ -2,4 +2,8 @@
 
 @section('content')
 HIDRO PROJEKT
+@if (Auth::user()->id == 5)
+<img src="{{ asset('images/happy.png') }}" class="rounded mx-auto d-block mt-2" alt="..." id="currentCarPic" style="height:250px; width:auto;max-width: 644px">   
+@endif
+
 @endsection
