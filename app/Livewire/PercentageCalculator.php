93c7205251f39c0a405e4c65cfd034583c41fcfb
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-04-16 11:42:41 +0200

    updated calculator

--- app/Livewire/PercentageCalculator.php
+++ app/Livewire/PercentageCalculator.php
@@ -7,8 +7,14 @@ use Livewire\Component;
 class PercentageCalculator extends Component
 {
     public $type;
+    public $percentageType = 'add';
     public $amount = [];
     public $percentage = 3;
+    public $text = [];
+
+    public function mount(){
+        $this->setText();
+    }
 
     public function updatedAmountAmount1($key){
         if($key != ""){
@@ -17,6 +23,10 @@ class PercentageCalculator extends Component
             $this->amount['amount1'] = 0;
             $this->amount['amount2'] = 0;
         } 
+    }
+
+    public function updatedPercentageType(){
+        $this->calculate();
     }
 
     public function updatedPercentage($key){
@@ -29,13 +39,32 @@ class PercentageCalculator extends Component
         }     
     }
 
+    private function getPercentageValue(){
+        if($this->percentageType == 'add'){
+            return $this->amount['amount2'] = $this->amount['amount1'] * (1 + $this->percentage/100);
+        }
+        if($this->percentageType == 'deduct'){
+           return  $this->amount['amount2'] = $this->amount['amount1'] * (1 - $this->percentage/100);
+        }
+        return;
+    }
 
     public function calculate(){
         if($this->type == 'add'){
-            return $this->amount['amount2'] = $this->amount['amount1'] * (1 + $this->percentage/100);
+            $this->getPercentageValue();
         }
         if($this->type == 'remove'){
-           return  $this->amount['amount2'] = $this->amount['amount1'] * (1 - $this->percentage/100);;
+           return  $this->amount['amount2'] = $this->amount['amount1'] * ($this->percentage/100);
+        }
+        return;
+    }
+
+    private function setText(){
+        if($this->type == 'add'){
+            return $this->text['text01'] = 'Uvečaj/smanji';
+        }
+        if($this->type == 'remove'){
+            return $this->text['text01'] = '%:';
         }
         return;
     }
--- resources/views/livewire/percentage-calculator.blade.php
+++ resources/views/livewire/percentage-calculator.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="row mb-2">
-        <div class="col col-md-5">
+        <div class="col col-md-@if($type=='add') 4 @else 5 @endif">
             <div class="form-group">
                 <label for="amount1">Iznos</label>
                 <input type="number" class="form-control" id="amount1" wire:model.blur='amount.amount1'>
@@ -8,16 +8,33 @@
         </div>
         <div class="col col-md-2">
             <div class="form-group">
-                <label for="percentage">@if($type=='add') Uvečaj za %:@endif @if($type=='remove') Smanji za %:@endif</label>
+                <label for="percentage">{{ $text['text01'] }}</label>
                 <input type="number" class="form-control" id="percentage" wire:model.blur='percentage'>
             </div>
         </div>
-        <div class="col col-md-5">
+        @if ($type=='add')
+        <div class="col col-md-1 pt-2">
+            <div class="form-check">
+                <input class="form-check-input" type="radio" value="add" wire:model.live='percentageType'>
+                <label class="form-check-label" for="exampleRadios1">
+                    +
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" value="deduct" wire:model.live='percentageType'>
+                <label class="form-check-label" for="exampleRadios2">
+                -
+                </label>
+            </div>
+        </div>
+        @endif
+        <div class="col col-md-@if($type=='add') 4 @else 5 @endif">
             <div class="form-group">
                 <label for="amount2">Rezultat</label>
                 <input type="number" class="form-control" id="amount2" disabled wire:model.blur='amount.amount2'>
             </div>
         </div>
+        
     </div>
 </div>
 
