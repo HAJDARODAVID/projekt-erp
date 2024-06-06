61f01577bd4eb1fc576cc73775ef455b836ca57e
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-06-06 10:14:51 +0200

    Booking main inventory now in function

--- app/Services/HidroProjekt/ADM/MainInventoryService.php
+++ app/Services/HidroProjekt/ADM/MainInventoryService.php
@@ -53,7 +53,6 @@ class MainInventoryService
     public function bookMainInventory($activeInventory){
         $oldStockItems = StorageStockItem::where('qty','>', 0)->get();
         $invItems = InventoryCheckingItem::where('inv_id', $activeInventory->id)->get();
-
         $newMovement = MaterialDocModel::create([
             'mvt_type' => MovementTypes::BOOK_INVENTORY_REMOVE_STOCK,
             'created_by' => Auth::user()->id,
@@ -62,21 +61,21 @@ class MainInventoryService
         foreach ($oldStockItems as $item) {
             //Archive old stock item
             StorageStockItemsHist::create([
-                'inv_id' => $activeInventory->id,
-                'mat_id' => $item->mat_id,
+                'inv_id'  => $activeInventory->id,
+                'mat_id'  => $item->mat_id,
                 'str_loc' => $item->str_loc,
                 'cons_id' => $item->cons_id,
-                'qty' => $item->qty,
+                'qty'     => $item->qty,
             ]);
 
             //Add to material movement table
             MaterialMvtModel::create([
-                'stg_loc' => $item->str_loc, 
-                'const_id' => $item->cons_id, 
-                'mvt' => $newMovement->mvt_type, 
-                'mat_doc_id' => $newMovement->id, 
-                'mat_id' => $item->mat_id, 
-                'qty' => $item->qty,
+                'stg_loc'    => $item->str_loc,
+                'const_id'   => $item->cons_id,
+                'mvt'        => $newMovement->mvt_type,
+                'mat_doc_id' => $newMovement->id,
+                'mat_id'     => $item->mat_id,
+                'qty'        => $item->qty,
             ]);
 
             //Set stoke item to 0 
@@ -84,8 +83,46 @@ class MainInventoryService
                 'qty' => 0,
             ]);
         }
+
+        $newMovement = MaterialDocModel::create([
+            'mvt_type' => MovementTypes::BOOK_INVENTORY_ADD_STOCK,
+            'created_by' => Auth::user()->id,
+        ]);
+        foreach($invItems as $item){
+            //Add to stock items
+            $ssi = StorageStockItem::where('mat_id', $item->mat_id) 
+                ->where('str_loc', $item->str_loc)
+                ->where('cons_id', $item->cons_id)
+                ->first();
+            if($ssi){
+                $ssi->update([
+                    'qty' => $item->qty,
+                ]);
+            }else{
+                StorageStockItem::create([
+                    'mat_id'  => $item->mat_id,
+                    'str_loc' => $item->str_loc,
+                    'cons_id' => $item->cons_id,
+                    'qty'     => $item->qty,
+                ]);
+            };
+
+            //Add to material movement table
+            MaterialMvtModel::create([
+                'stg_loc'    => $item->str_loc,
+                'const_id'   => $item->cons_id,
+                'mvt'        => $newMovement->mvt_type,
+                'mat_doc_id' => $newMovement->id,
+                'mat_id'     => $item->mat_id,
+                'qty'        => $item->qty,
+            ]);
+        }
+
+        $activeInventory->update([
+            'status' => InventoryCheckingModel::INVENTORY_STATUS_BOOKED,
+        ]);
+
         return true;
-        dd($invItems);
     }
 
 }
