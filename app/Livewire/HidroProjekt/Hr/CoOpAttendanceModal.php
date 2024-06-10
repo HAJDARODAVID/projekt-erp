413700ab830ddd0e3cdb23faa943a5659ea08f3e
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-06-10 08:50:54 +0200

    created new modal component for coOp attendance hours

---
+++ app/Livewire/HidroProjekt/Hr/CoOpAttendanceModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\HidroProjekt\Hr;
+
+use Livewire\Component;
+
+class CoOpAttendanceModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.hidro-projekt.hr.co-op-attendance-modal');
+    }
+}
