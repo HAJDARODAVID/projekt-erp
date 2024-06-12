c4a8994006cdfeb872685d098ebee02739dee3ae
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-06-12 14:10:04 +0200

    created new components

---
+++ app/Livewire/HidroProjekt/Hr/WorkerAttendanceModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\HidroProjekt\Hr;
+
+use Livewire\Component;
+
+class WorkerAttendanceModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.hidro-projekt.hr.worker-attendance-modal');
+    }
+}
