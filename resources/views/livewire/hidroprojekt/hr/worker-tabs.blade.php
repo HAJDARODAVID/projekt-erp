1d61c458328d8de8dbb67afaa5d745d4c44deab8
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-06-26 10:46:45 +0200

    fixing live

--- resources/views/livewire/hidroprojekt/hr/worker-tabs.blade.php
+++ resources/views/livewire/hidroprojekt/hr/worker-tabs.blade.php
@@ -93,7 +93,7 @@
                     <div class="row mt-1 mb-2">
                         <div class="col-3">
                             <div class="form-check form-switch mt-1">
-                                <input class="form-check-input" type="checkbox" wire:model.live='payrollInfo.bonus'>
+                                <input class="form-check-input" type="checkbox" wire:model='payrollInfo.bonus'>
                                 <label class="form-check-label" for="bonus">
                                   Da li radnik može dobiti bonus? 
                                 </label>
