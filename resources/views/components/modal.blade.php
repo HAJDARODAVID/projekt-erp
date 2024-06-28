64d7e5f04dc56890b991035191f27c17a6d22f96
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-06-28 08:44:37 +0200

    started to make a model component

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,18 @@
+<div class="modal" id="changeWorkerHRate" style="display: @if($showModal) block @endif">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title" id="exampleModalLabel">
+                    Promjena satnice radnika
+                </h6>
+            </div>
+            <div class="modal-body">
+                body
+            </div>
+            <div class="modal-footer">
+                footer
+            </div>
+
+        </div>
+    </div>
+</div>
