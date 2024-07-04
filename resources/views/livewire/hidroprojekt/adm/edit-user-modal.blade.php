8cd85e5fac1a41125d4e2dae84d13e2d67d5f160
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-07-04 06:33:29 +0200

    added modal component to edit user

--- resources/views/livewire/hidroprojekt/adm/edit-user-modal.blade.php
+++ resources/views/livewire/hidroprojekt/adm/edit-user-modal.blade.php
@@ -7,64 +7,54 @@
         UREDI
     </button>
 
-    <div class="modal" id="editUser" style="display:  @if ($modalStatus) block @else none @endif">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header d-flex justify-content">
-                    <div>
-                        <h5 class="modal-title" id="exampleModalLabel">
-                            Uređivanje korisnika: {{ $row->name }}
-                        </h5>
-                    </div>
-                    <div>
-                        <button class="btn btn-dark btn-sm" wire:click='modalBtn(0)' wire:loading.attr='disabled'>X</button>
-                    </div>
+    <x-modal :show='$modalStatus' :blur='TRUE'>
+        <x-slot name='mainTitle'>Uređivanje korisnika: {{ $row->name }}</x-slot>
+        <x-slot name='headerBtn'> 
+            <button class="btn btn-dark btn-sm" wire:click='modalBtn(0)' wire:loading.attr='disabled'>X</button>
+        </x-slot>
+        {{-- BODY CONTEND --}}
+        <div class="d-flex justify-content-center">
+            @if (!$row->active)
+                <button class="btn btn-success"
+                wire:click='activateUser()' 
+                wire:loading.attr='disabled' >
+                    AKTIVIRAJ!
+                </button>   
+            @endif
+            @if ($row->active)
+                <button class="btn btn-success"
+                wire:click='resetPassword()' 
+                wire:loading.attr='disabled' >
+                    RESET LOZINKE!
+                </button>   
+            @endif
+        </div> 
+    
+        <div class="form-group mb-2">
+            <label>Korisnik</label>
+            <input type="text" class="form-control" wire:model.blur='data.name'>
+        </div>
+        <div class="row mb-2">
+            <div class="col">
+                <div class="form-group">
+                    <label>Ime</label>
+                    <input type="text" class="form-control" wire:model='workerInfo.firstName' disabled>
                 </div>
-                <div class="modal-body">
-                    <div class="d-flex justify-content-center">
-                        @if (!$row->active)
-                            <button class="btn btn-success"
-                            wire:click='activateUser()' 
-                            wire:loading.attr='disabled' >
-                                AKTIVIRAJ!
-                            </button>   
-                        @endif
-                        @if ($row->active)
-                            <button class="btn btn-success"
-                            wire:click='resetPassword()' 
-                            wire:loading.attr='disabled' >
-                                RESET LOZINKE!
-                            </button>   
-                        @endif
-                    </div>
-
-                    <div class="form-group mb-2">
-                        <label>Korisnik</label>
-                        <input type="text" class="form-control" wire:model.blur='data.name'>
-                    </div>
-                    <div class="row mb-2">
-                        <div class="col">
-                            <div class="form-group">
-                                <label>Ime</label>
-                                <input type="text" class="form-control" wire:model='workerInfo.firstName' disabled>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="form-group">
-                                <label>Prezime</label>
-                                <input type="text" class="form-control" wire:model='workerInfo.lastName' disabled>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group mb-2">
-                        <label>E-mail</label>
-                        <input type="email" class="form-control" wire:model.blur='data.email'>
-                    </div>
-                    
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label>Prezime</label>
+                    <input type="text" class="form-control" wire:model='workerInfo.lastName' disabled>
                 </div>
             </div>
         </div>
-    </div>
+        <div class="form-group mb-2">
+            <label>E-mail</label>
+            <input type="email" class="form-control" wire:model.blur='data.email'>
+        </div>
+    
+    </x-modal>
 
     <x-processing-modal target='activateUser, resetPassword'></x-processing-modal>
 </div>
+
