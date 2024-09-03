1e9a4ab04779c7d5ab3495beb08de8621b2169cc
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-09-03 09:53:08 +0200

    syntax corection

--- resources/views/livewire/hidroprojekt/adm/inventory-checking-inputbox.blade.php
+++ resources/views/livewire/hidroprojekt/adm/inventory-checking-inputbox.blade.php
@@ -4,6 +4,6 @@
         <span style="cursor: pointer; display:@if($displayInput) none @endif" wire:click='openInputBox()'>
             {{ $qty }}
         </span>
-        <input id = type="number" class="form-control input-sm" style="display:@if($displayInput) block @else none @endif; width: 100px" wire:model.blur = 'qty'>
+        <input type="number" class="form-control input-sm" style="display:@if($displayInput) block @else none @endif; width: 100px" wire:model.blur = 'qty'>
     </div>
 </div>
