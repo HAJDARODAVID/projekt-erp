fc41309072d94d4e37b698c028e0c3d46284cd9a
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-09-11 08:07:32 +0200

    traing

--- app/Livewire/HidroProjekt/Wp/InternalDeliveryNoteConstructionSitesTable.php
+++ app/Livewire/HidroProjekt/Wp/InternalDeliveryNoteConstructionSitesTable.php
@@ -47,7 +47,9 @@ class InternalDeliveryNoteConstructionSitesTable extends DataTableComponent
 
     private function getConstructionSite($row){
         $constSite = MaterialMvtModel::where('mat_doc_id', $row->id)->where('const_id', '!=', NULL)->pluck('const_id')->toArray();
-        dd($constSite);
+        if(empty($constSite)){
+            dd('imam');
+        }
         $constSiteInfo = ConstructionSiteModel::where('id',$constSite[0])->first();
         return $constSiteInfo->name;
     }
