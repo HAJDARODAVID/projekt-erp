69229f3d2c6a5a3d83fe2400a74d8214dead58d1
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-09-18 13:50:06 +0200

    done with warning indicator

--- app/Livewire/WarningIndicator.php
+++ app/Livewire/WarningIndicator.php
@@ -3,9 +3,16 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class WarningIndicator extends Component
 {
+    public $show=false;
+
+    public function getIndicator(){
+        return $this->show = $this->show = Auth::user()->inv_update;
+    }
+
     public function render()
     {
         return view('livewire.warning-indicator');
--- resources/views/livewire/warning-indicator.blade.php
+++ resources/views/livewire/warning-indicator.blade.php
@@ -1,3 +1,3 @@
-<div style="display: block">
+<div style="display: @if($show) block @else none @endif" wire:poll.600s='getIndicator()'>
     <i class="bi bi-exclamation-triangle-fill" style="color: yellow"></i>
 </div>
