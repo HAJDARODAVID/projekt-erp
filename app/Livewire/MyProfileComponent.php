f37f7ed51c1518e358e12ebe01592593eea84be3
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-09-26 15:02:48 +0200

    name comes from db - and default avatar pic

--- app/Livewire/MyProfileComponent.php
+++ app/Livewire/MyProfileComponent.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class MyProfileComponent extends Component
 {
+    public $userName;
+
+    public function mount(){
+        $this->userName = User::where('id', Auth::user()->id)->with('getWorker')->first()->getWorker->fullName; 
+    }
+
     public function render()
     {
         return view('livewire.my-profile-component');
--- resources/views/livewire/my-profile-component.blade.php
+++ resources/views/livewire/my-profile-component.blade.php
@@ -1,16 +1,17 @@
 <div class="mt-auto px-2 pb-3">
   <hr>
   <div class="dropdown" onmouseover="document.getElementById('profil-menu').classList.add('show');">
-    <span class="d-flex align-items-center text-decoration-none dropdown-toggle">
-      <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+    {{-- ADD LATER TO SPAN --> dropdown-toggle --}}
+    <span class="d-flex align-items-center text-decoration-none">
+      <img src="{{ asset('images/user_avatar/default.png') }}" alt="" width="32" height="32" class="rounded-circle me-2">
       <strong >
-        David Hajdarović
+        {{ $userName }}
       </strong>
     </span>
-    <ul id="profil-menu" class="dropdown-menu text-small text-black shadow mx-1 mb-1" style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate(0px, -34px);" onmouseout="document.getElementById('profil-menu').classList.remove('show');">
+    {{-- <ul id="profil-menu" class="dropdown-menu text-small text-black shadow mx-1 mb-1" style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate(0px, -34px);" onmouseout="document.getElementById('profil-menu').classList.remove('show');">
       <li><a class="dropdown-item" href="#">Profile</a></li>
       <li><hr class="dropdown-divider"></li>
       <li><a class="dropdown-item" href="#">Sign out</a></li>
-    </ul>
+    </ul> --}}
   </div>
 </div>
