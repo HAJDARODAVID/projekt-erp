7faf43c2967745bc5cfcdb2eaac0d0dfc4ecb937
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-09-26 08:28:08 +0200

    activated new menu, and reduced poll for warning indicator

--- resources/views/layouts/mainAdminLayout.blade.php
+++ resources/views/layouts/mainAdminLayout.blade.php
@@ -117,13 +117,9 @@
   <div class="row">
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
       <div class="position-sticky pt-3 sidebar-sticky">
-
-        @if (Auth::user()->id == 1 || Auth::user()->id == 15)
           @livewire('main-menu')  
-        @else
-          <x-admin-module-menu-items></x-admin-module-menu-items>
-        @endif
-        
+          {{-- OLD MENU - BACK UP --}}
+          {{-- <x-admin-module-menu-items></x-admin-module-menu-items> --}}
       </div>
     </nav>
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
--- resources/views/livewire/warning-indicator.blade.php
+++ resources/views/livewire/warning-indicator.blade.php
@@ -1,3 +1,3 @@
-<div style="display: @if($show) block @else none @endif" wire:poll.600s='getIndicator()'>
+<div style="display: @if($show) block @else none @endif" wire:poll.300s='getIndicator()'>
     <i class="bi bi-exclamation-triangle-fill" style="color: yellow"></i>
 </div>
