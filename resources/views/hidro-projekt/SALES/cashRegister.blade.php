462805495fa56ef9ea49e11436faf4d1317d7335
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-10-10 12:36:24 +0200

    design seams ok

--- resources/views/hidro-projekt/SALES/cashRegister.blade.php
+++ resources/views/hidro-projekt/SALES/cashRegister.blade.php
@@ -9,20 +9,51 @@
     @endif
   
     <div class="container">
+        <div class="d-flex mx-3">
+            <div class="h5"><b>INTERNA BLAGAJNA</b></div>
+        </div>
         <div class="row">
             <div class="col-sm-4 p-3">
-                <div class="alert alert-primary m-0">
-                    This is a primary alert—check it out!
+                <div class="alert alert-light m-0 mb-4 shadow">
+                    <div class="form-group mb-3 mx-2">
+                        <label for="">Kupca</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <hr class="m-0 my-2">
+                    <div class="form-group mb-3 mx-2">
+                        <label for="">Način plačanja</label>
+                        <input type="text" class="form-control" placeholder="Kupac">
+                    </div>
+                    <div class="form-group mb-3 mx-2">
+                        <label for="">Status plačanja</label>
+                        <input type="text" class="form-control" placeholder="Kupac">
+                    </div>
+                </div>
+                <div class="alert alert-light m-0 shadow">
+                    <div class="form-group mb-3 mx-2">
+                        <label for="">Ukupni iznos</label>
+                        <input type="text" class="form-control" disabled>
+                    </div>
+                    <hr class="m-0 my-2">
+                    <div class="form-group mb-3 mx-2">
+                        <label for="">Način plačanja</label>
+                        <input type="text" class="form-control" placeholder="Kupac">
+                    </div>
+                    <div class="form-group mb-3 mx-2">
+                        <label for="">Status plačanja</label>
+                        <input type="text" class="form-control" placeholder="Kupac">
+                    </div>
                 </div>
             </div>
             <div class="col-sm p-3">
-                <div class="d-flex mb-2">
-                    <div class="form-group mb-2 mx-2" style="width: 250px" >
-                        <input type="number" class="form-control" placeholder="Br. materijala">
-                    </div>
-                    <button class="btn btn-success align-items-center" style="height: 38px"><i class="bi bi-plus-circle"></i></button>
-                </div>
                 <div class="alert alert-light shadow m-0" style="height: 600px">
+                    <div class="d-flex mb-2">
+                        <div class="form-group mb-2 mx-2" style="width: 250px" >
+                            <input type="number" class="form-control" placeholder="Br. materijala">
+                        </div>
+                        <button class="btn btn-success align-items-center" style="height: 38px"><i class="bi bi-plus-circle"></i></button>
+                    </div>
+                    <hr class="m-0 my-2">
                     <table class="table">
                         <thead>
                             <tr>
