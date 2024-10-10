b9f62c0a9ac58e8c22d4fc646724e26a3f94ac97
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-10-10 13:41:38 +0200

    moved to livewire

---
+++ app/Livewire/HidroProjekt/Sales/RegisterComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\HidroProjekt\Sales;
+
+use Livewire\Component;
+
+class RegisterComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.hidroprojekt.sales.register-component');
+    }
+}
