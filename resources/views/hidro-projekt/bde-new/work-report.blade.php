1d2fdceb0ec4fd04c5669aed8fc626cdf4d6111d
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-11-26 08:33:31 +0100

    clear local storage on every new wor report creation

--- resources/views/hidro-projekt/bde-new/work-report.blade.php
+++ resources/views/hidro-projekt/bde-new/work-report.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+@if ($clear)
+    <script>localStorage.clear();</script>
+@endif
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
