9d39fc3b63c0ad1b4b6a30bee5d3185a2e746f1f
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-11-29 10:52:52 +0100

    reverted changes

--- app/Livewire/HidroProjekt/Wp/InternalDeliveryNoteConstructionSitesTable.php
+++ app/Livewire/HidroProjekt/Wp/InternalDeliveryNoteConstructionSitesTable.php
@@ -2,10 +2,9 @@
 
 namespace App\Livewire\HidroProjekt\Wp;
 
+use App\Models\ConstructionSiteModel;
 use App\Models\MaterialDocModel;
 use App\Models\MaterialMvtModel;
-use App\Models\ConstructionSiteModel;
-use App\Models\InternalDeliveryNoteView;
 use Illuminate\Database\Eloquent\Builder;
 use App\Services\HidroProjekt\STG\MovementTypes;
 use Rappasoft\LaravelLivewireTables\Views\Column;
@@ -29,22 +28,34 @@ class InternalDeliveryNoteConstructionSitesTable extends DataTableComponent
                 ->hideIf(TRUE),
             Column::make("Vrsta knjiženja")
                 ->label(
-                    fn($row, Column $column) => MovementTypes::MVT_DESC_HR[$row->mvt_type],
+                    fn($row, Column $column) => $row->fullName
                 ),
-            Column::make("Gradilište", 'name')
-                ->sortable()
-                ->searchable(),
-            Column::make("Izdavatelj", "created_by")
+            Column::make("Gradilište")
+                ->label(
+                    fn($row, Column $column) => $this->getConstructionSite($row)
+                ),
+            Column::make("Izdavatelj", "getUserInfo.name")
                 ->sortable(),
             Column::make("Stvoreno", "created_at")
                 ->sortable(),
+            Column::make("Ažurirano", "updated_at")
+                ->hideIf(TRUE),
             Column::make("Actions",'id')
                 ->view('hidro-projekt.WP.deliveryNoteTableBtn'),
         ];
     }
 
+    private function getConstructionSite($row){
+        $constSite = MaterialMvtModel::where('mat_doc_id', $row->id)->where('const_id', '!=', NULL)->pluck('const_id')->toArray();
+        if(empty($constSite)){
+            return null;
+        }
+        $constSiteInfo = ConstructionSiteModel::where('id',$constSite[0])->first();
+        return $constSiteInfo->name;
+    }
+
     public function builder(): Builder{
-        return InternalDeliveryNoteView::query()
+        return MaterialDocModel::query()
             ->whereIn('mvt_type', MovementTypes::CONST_SITE_MVT)
             ->orderBy('id', 'DESC');
     }
