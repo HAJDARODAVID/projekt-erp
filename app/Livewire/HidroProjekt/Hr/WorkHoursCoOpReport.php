0f61eab91ef64f226b5b3b9cd2d0b12204daab18
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-12-02 13:16:37 +0100

    still fixing

--- app/Livewire/HidroProjekt/Hr/WorkHoursCoOpReport.php
+++ app/Livewire/HidroProjekt/Hr/WorkHoursCoOpReport.php
@@ -7,19 +7,24 @@ use App\Services\Months;
 use Livewire\Attributes\On; 
 use App\Services\HidroProjekt\HR\WorkHoursService;
 use App\Livewire\HidroProjekt\Hr\CoOpWorkHoursExportModal;
+use App\Services\Years;
 
 class WorkHoursCoOpReport extends Component
 {
     public $selectedYear;
     public $selectedMonth;
     public $months = Months::MONTHS_HR;
+    public $years;
     public $daysOfMonth;
     public $attendance;
 
     #[On('refreshWorkHoursComponent')] 
     public function mount(){
+        $this->years = Years::getYearsList();
         $this->daysOfMonth=Months::dayOfMonth($this->selectedMonth, $this->selectedYear);
         $this->attendance = WorkHoursService::getAllAttendanceForMonthReportCoOp($this->selectedMonth, $this->selectedYear,$this->daysOfMonth);
+
+        //dd( $this->attendance);
     }
 
     public function openAttendanceModalForWorkerAndDay($workerId=NULL, $date=NULL){
