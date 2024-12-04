05e4e4812e833b48d7b359ad14821f7d02d4b791
David Hajdarovic <hajdarovicdavid@gmail.com>
2024-12-04 11:29:12 +0100

    changed title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Hidro Projekt</title>
+    <title>Hidro-projekt</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
