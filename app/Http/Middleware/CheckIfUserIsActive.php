314b513d0b85978be2e2c59aa58c000558cf3e71
David Hajdarovic <hajdarovicdavid@gmail.com>
2025-04-07 14:15:01 +0200

    testing live

--- app/Http/Middleware/CheckIfUserIsActive.php
+++ app/Http/Middleware/CheckIfUserIsActive.php
@@ -16,6 +16,7 @@ class CheckIfUserIsActive
      */
     public function handle(Request $request, Closure $next): Response
     {
+        dd(Auth::user());
         if(Auth::user()->active != 1){
             return redirect()->route('home');
         }
