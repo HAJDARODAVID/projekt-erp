ddbf2b1070690e54f0f1fec899ba8250706c8951
David Hajdarovic <hajdarovicdavid@gmail.com>
2025-05-06 12:57:42 +0200

    added the processing spinner

--- resources/views/livewire/domain/storage/storage-excel-export.blade.php
+++ resources/views/livewire/domain/storage/storage-excel-export.blade.php
@@ -1,3 +1,4 @@
 <div>
     <button class="btn btn-success" wire:click='getExportData()'><i class="bi bi-file-earmark-spreadsheet"></i></button>
+    <x-processing-modal target='getExportData'></x-processing-modal>
 </div>
