616e1bf2635f3770bbcc1c69624f1453854d5667
David Hajdarovic <hajdarovicdavid@gmail.com>
2025-05-12 08:40:07 +0200

    deployer done

--- deploy.php
+++ deploy.php
@@ -3,18 +3,78 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
-// Config
+set('application', 'dep');
 
-set('repository', 'https://github.com/HAJDARODAVID/projekt-erp.git');
+// Project repository
+set('repository', 'git@github.com:HAJDARODAVID/projekt-erp.git');
 
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
 add('shared_files', []);
 add('shared_dirs', []);
-add('writable_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', [
+    'storage',
+    'bootstrap',
+]);
 
 // Hosts
+host('HP_Q-sys')
+    ->setHostname('64.225.101.184')
+    ->setRemoteUser('deploy')
+    ->setIdentityFile('~/.ssh/id_rsa')
+    ->set('deploy_path', '/var/www/hidro-projekt-test')
+    ->set('branch', 'master')
+    ->set('ssh_multiplexing', true);
 
-host('64.225.101.184')
-    ->set('remote_user', 'root')
-    ->set('deploy_path', 'var/www/hidro-projekt');
+host('HP_P-sys')
+    ->setHostname('64.225.101.184')
+    ->setRemoteUser('deploy')
+    ->setIdentityFile('~/.ssh/id_rsa')
+    ->set('deploy_path', '/var/www/hidro-projekt')
+    ->set('branch', 'master')
+    ->set('ssh_multiplexing', true);
 
+// Custom task to check condition and run migration
+task('deploy:conditionally_migrate', function () {
+    // Define your condition here
+    $shouldMigrate = false; // Replace with your actual condition
+
+    if ($shouldMigrate) {
+        invoke('artisan:migrate');
+    } else {
+        writeln('Skipping migration as the condition is not met.');
+    }
+});
+// Tasks
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+
+desc('Custom Composer install');
+task('deploy:vendors', function () {
+    writeln('Running custom Composer install...');
+    if (!commandExist('unzip')) {
+        warning('To speed up composer installation, setup "unzip" command with PHP zip extension.');
+    }
+    run('
+        cd {{release_or_current_path}} && 
+        {{bin/composer}} install --ignore-platform-req=ext-zip && 
+        npm install &&
+        rm -rf package-lock.json node_modules &&
+        npm cache clean --force #use --force &&
+        npm i && npm run build');
+});
+
+// Hook the custom deploy:vendors task
+//after('deploy:update_code', 'deploy:vendors');
+
+// [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+before('deploy:symlink', 'deploy:conditionally_migrate');
