7ff5f88b6c344f4ecb0b7c5c7d27946e838d828b
David Hajdarovic <hajdarovicdavid@gmail.com>
2025-07-31 11:51:04 +0200

    some updated

---
+++ app/Livewire/Modules/WorkdayDiary/MainTable.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Livewire\Modules\WorkdayDiary;
+
+use App\Models\User;
+use App\Models\WorkDayDiaryViewModel;
+use Illuminate\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+
+class MainTable extends DataTableComponent
+{
+    //protected $model = WorkDayDiaryViewModel::class;
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("#", "id")
+                ->sortable(),
+            Column::make("user_id", "user_id")
+                ->hideIf(TRUE),
+            Column::make("Korisnik")
+                ->label(
+                    fn($row, Column $column) => $this->getUserName($row)
+                ),
+            Column::make("Gradilište", "name")
+                ->sortable(),
+            Column::make("Datum", "date")
+                ->sortable(),
+            Column::make("Vrsta terena", "work_type")
+                ->sortable(),
+            Column::make("Vozilo", "car_plates")
+                ->sortable(),
+        ];
+    }
+
+    public function builder(): Builder
+    {
+        return WorkDayDiaryViewModel::query()
+            ->where('construction_site_id', '!=', NULL)
+            ->orderBy('date', 'desc');
+    }
+
+    public function getUserName($row)
+    {
+        $user = User::where('id', $row->user_id)->with('getWorker', 'getCooperator')->first();
+        if (!is_null($user->getWorker)) return $user->getWorker->fullName;
+        if (!is_null($user->getCooperator)) return $user->getCooperator->fullName;
+    }
+}
--- resources/views/livewire/modules/workday-diary/index.blade.php
+++ resources/views/livewire/modules/workday-diary/index.blade.php
@@ -3,6 +3,6 @@
         <x-ui.module.header title="DNEVNIK RADOVA">
             <x-slot:actionsBtn> <x-ui.btn type="suc.sm" icon="file-earmark-plus"></x-ui.btn> </x-slot>
         </x-ui.module-header>
-        <livewire:hidroProjekt.wp.work-diary-table theme="bootstrap-5" />
+        @livewire('modules.workday-diary.main-table')
     </div>
 </div>
