5b45bf3657de1ab142e60556eaa714747c4203d4
David Hajdarovic <hajdarovicdavid@gmail.com>
2026-03-31 17:09:05 +0200

    drop update

--- database/migrations/2024_10_02_104244_update_construction_site_consumption_view.php
+++ database/migrations/2024_10_02_104244_update_construction_site_consumption_view.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+        $this->down();
         DB::statement("
-            DROP VIEW IF EXISTS construction_site_consumption_view;
             CREATE VIEW construction_site_consumption_view AS
             (
                 SELECT 
